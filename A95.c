be37c7d438bd537d16a7e39f8992398f946065b0
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-15 16:02:17 +0530

    Create A95.c

---
+++ A95.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+#include<conioh>
+int main(void)
+{
+	float amt,rate,time,in;
+	printf("amt,rate,time\n");
+	scanf("%f%f%f",&amt,&rate,&time);
+	interest=(amt*rate*time)/100;
+	printf(" %f",in);
+	
+	return 0;
+}
