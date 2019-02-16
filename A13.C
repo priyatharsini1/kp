5b0c4649776c0d4f2118f3ceda377630a0a96eb1
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-16 15:27:07 +0530

    Create A13.C

---
+++ A13.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() 
+{
+int n,i;
+scanf("%d",&n);
+int flag=1;
+for(i=2;i<n/2;i++)
+{
+
+    if(n%i==0)
+    {
+       flag=0;
+    }
+    else
+    {
+       flag=1;
+    }
+}
+if(flag==1)
+{
+    printf(" prime");
+}
+else
+{
+    printf(" not prime");
+}
+}
