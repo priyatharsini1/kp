4afc70254fdf942ff22ae858e346c6d817fc1198
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-18 21:14:51 +0530

    Create B3A.C

---
+++ B3A.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() 
+{
+
+int n,remainder,r=0;
+
+int result;
+
+scanf("%d",&n);
+
+while(n!=0)
+{
+remainder=n%10;
+r=r*10+remainder;
+n=n/10;
+}
+printf("\n%d",r);
+
+return 0;
+
+ 
+
+}
