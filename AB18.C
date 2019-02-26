76aa1a4c05aa14bdd07b368805d82bdab105bf1d
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-26 20:21:26 +0530

    Create AB18.C

---
+++ AB18.C
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void sort(int[],int);
+int main()
+{
+int n1,n2,e[10],f[10],i,count=0;
+char a[10],b[10];
+clrscr();
+printf("Enter two string:");
+scanf("%s  %s",&a,&b);
+n1=strlen(a);
+n2=strlen(b);
+if(n1==n2)
+{
+for(i=0;i<n1;i++)
+{
+e[i]=a[i];
+}
+for(i=0;i<n2;i++)
+{
+f[i]=b[i];
+}
+sort(e,n1);
+sort(f,n2);
+for(i=0;i<n1;i++)
+{
+if(e[i]!=f[i])
+{
+count=1;
+break;
+}
+}
+if(count==1)
+{
+printf("  The given string is not anagram");
+}
+else
+{
+printf(" The given string is anagram");
+}
+}
+else
+{
+printf("  The given string is not anagram");
+}
+getch();
+}
+void sort(int a[],int n)
+{
+int i,j,t;
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+return 0;
+}
